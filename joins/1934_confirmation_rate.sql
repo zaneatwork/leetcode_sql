9cf078edf058eb380a2c5b873b1fcce2e0549790
Zane <91981951+zaneatwork@users.noreply.github.com>
2024-07-26 17:07:35 -0500

    adding problems 1075 and 1251. Finished renaming files to new format

---
+++ joins/1934_confirmation_rate.sql
@@ -0,0 +1,45 @@
+-- Table: Signups
+--
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- user_id is the column of unique values for this table.
+-- Each row contains information about the signup time for the user with ID 
+-- user_id.
+--
+--  
+--
+-- Table: Confirmations
+--
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- | action         | ENUM     |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique 
+-- values) for this table.
+-- user_id is a foreign key (reference column) to the Signups table.
+-- action is an ENUM (category) of the type ('confirmed', 'timeout')
+-- Each row of this table indicates that the user with ID user_id requested a 
+-- confirmation message at time_stamp and that confirmation message was either 
+-- confirmed ('confirmed') or expired without confirming ('timeout').
+--
+--  
+--
+-- The confirmation rate of a user is the number of 'confirmed' messages divided 
+-- by the total number of requested confirmation messages. The confirmation rate 
+-- of a user that did not request any confirmation messages is 0. Round the 
+-- confirmation rate to two decimal places.
+--
+-- Write a solution to find the confirmation rate of each user.
+--
+-- Return the result table in any order.
+SELECT Signups.user_id, 
+  ROUND(AVG(CASE WHEN Confirmations.action = 'confirmed' THEN 1 ELSE 0 END),2) AS confirmation_rate
+FROM Confirmations FULL OUTER JOIN Signups ON Signups.user_id = Confirmations.user_id
+GROUP BY Signups.user_id
